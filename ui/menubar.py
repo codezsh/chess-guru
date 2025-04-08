4e50a861d343dff4c7268a85b61383f78d85fda8
codezsh <celestialrub@gmail.com>
2025-04-08 15:13:38 +0530

    added some menus

--- ui/menubar.py
+++ ui/menubar.py
@@ -1,35 +1,46 @@
 from PyQt6.QtWidgets import QMenuBar, QMenu
 from PyQt6.QtGui import QAction
-from PyQt6.QtCore import Qt
 from core.eventbus import Appbus
+
 
 class MenuBar(QMenuBar):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        board_menu = QMenu("Board", self)
-        flip_action = QAction("Flip Board", self)
-        
+        board_actions = [
+            (QAction("Flip Board", self), None),
+            (QAction("Reset Board", self), None),
+            (QAction("Load Position", self), None),
+            (QAction("Load game", self), None),
+        ]
 
-        flip_action.triggered.connect(lambda: Appbus.emit("flip_board"))
-        board_menu.addAction(flip_action)
-        
-        self.addMenu(board_menu)
+        analysis_actions = [
+            (QAction("Analysis", self), None),
+            (QAction("Top lines", self), None),
+        ]
 
+        learn_actions = [
+            (QAction("Opening", self), None),
+            (QAction("End games", self), None),
+            (QAction("Patterns", self), None),
+        ]
 
-        self.setStyleSheet("""
+        menus = [(QMenu("Board", self), board_actions), (QMenu("Analysis", self), analysis_actions), (QMenu("Learn", self), learn_actions)]
+
+        self.load_menus(menus)
+        self.setStyleSheet(
+            """
         QMenu {
-            padding: 6px;
+            padding: 5px;
             border-radius: 0px;
-            border:1px solid #4a4a4a;
-        }
+            border: 1px solid rgba(0, 0, 0, 50);
+        }  
+        """
+        )
 
-        QMenu::item {
-            padding: 5px 25px 5px 20px;
-        }
-
-        QMenu::item:selected {
-            background-color: #3d3d3d;
-        }
-    
-        """)
+    def load_menus(self, items):
+        for menu, actions in items:
+            for action, callback in actions:
+                # action.triggered.connect(callback)
+                menu.addAction(action)
+            self.addMenu(menu)
